dd5e0d512d57b8799ef74b6006d5b417eadc22dc
tpersse <thomaswpersse@gmail.com>
2024-05-23 09:18:21 -0700

    change paths for better desc.

--- callAlleleStatus/runAlleleStatus.sh
+++ callAlleleStatus/runAlleleStatus.sh
@@ -2,4 +2,4 @@
 
 # see script for environment dependencies
 
-/usr/bin/time -v python3 callAlleleStatusMain.py -v /path/to/annovar/snv/vcf/file.vcf -c /path/to/ploidy/adjusted/cn/calls.txt -i /path/to/indel/calls/dir/ -o /path/to/output/directory/ -loh /path/to/loh/calls/from/makeMatrix/endswithLOH.txt -gf /path/to/transcript/file.txt -ccg /path/to/list/of/cosmic/census/genes.tsv -sv /path/to/annotated/sv/calls/dir/
+/usr/bin/time -v python3 callAlleleStatusMain.py -v /path/to/annovar/snv/dir -c /path/to/ploidy/adjusted/cn/calls.txt -i /path/to/indel/calls/dir/ -o /path/to/output/directory/ -loh /path/to/loh/calls/from/makeMatrix/endswithLOH.txt -gf /path/to/transcript/file.txt -ccg /path/to/list/of/cosmic/census/genes.tsv -sv /path/to/annotated/sv/calls/dir/
